1c7c86b49b8617816b0dde045ac0096eb2b6530f
balajidamodharan <trybala29@gmail.com>
2019-05-21 10:59:21 +0530

    Program

---
+++ src/org/facebook/Loginpage.java
@@ -0,0 +1,18 @@
+package org.facebook;
+
+public class Loginpage {
+public static void main(String[] args) {
+	String s="Balaji D";
+	char c = s.charAt(1);
+	System.out.println(c);
+	int length = s.length();
+	System.out.println(length);
+	String upperCase = s.toUpperCase();
+	System.out.println(upperCase);
+	String lowerCase = s.toLowerCase();
+	System.out.println(lowerCase);
+	 String s1="Balaji D";
+	 boolean equals = s.equals(s1);
+	 System.out.println(equals);
+}
+}
